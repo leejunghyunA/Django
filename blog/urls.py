4d88024f20f300983fe08cdf802668be4d53af19
leejunghyunA <jeounghyun48@gmail.com>
2022-08-04 12:43:47 +0900

    CBV로 블로그 상세페이지 만들기

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()),
+    # path('<int:pk>/', views.single_post_page),
+]
